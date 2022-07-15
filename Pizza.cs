ee6ab31009830362baef6e913520d25ccd2e8936
stefanom13 <stefanomancuso13@gmail.com>
2022-07-15 12:34:06 +0200

    creato pizzaController  e modificato mapping default del controller

--- Pizza.cs
+++ Pizza.cs
@@ -2,5 +2,19 @@
 {
     public class Pizza
     {
+        public int Id { get; set; }
+        public string NomePizza { get; set; }
+        public string Descrizione { get; set; }
+        public string PathImage { get; set; }
+        public float Prezzo { get; set; }
+
+        public Pizza(int id, string nomePizza, string descrizione, string pathImage, float prezzo)
+        {
+            Id = id;
+            NomePizza = nomePizza;
+            Descrizione = descrizione;
+            PathImage = pathImage;
+            Prezzo = prezzo;
+        }
     }
 }
